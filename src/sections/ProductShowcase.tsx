ff77fd68f449680b58bfd664d5321b50109aa1a1
ahammoud <ahammoud@PORT2024090201>
2025-01-05 18:03:26 +0200

    Product showcase animation

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,12 +1,25 @@
+'use client'
+
 import ProductImage from '@/assets/product-image.png';
 import PyramidImage from '@/assets/pyramid.png';
 import TubeImage from '@/assets/tube.png';
 import Image from 'next/image';
-
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 export const ProductShowcase = () => {
+
+  const sectionRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ['start end', 'end start']
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className='bg-gradient-to-b from-[#fff] to-[#d2dcff] py-24 overflow-x-clip'>
+    <section ref={sectionRef} className='bg-gradient-to-b from-[#fff] to-[#d2dcff] py-24 overflow-x-clip'>
       <div className="container">
         <div className="section-heading">
           <div className="flex justify-center">
@@ -18,9 +31,30 @@ export const ProductShowcase = () => {
           <p className='section-description mt-5'>Effortlessly turn your ideas into a fully functional, responsive SaaS website in just minutes with this template.</p>
         </div>
         <div className="relative">
-          <Image src={ProductImage} alt='Product image' className='mt-10' />
-          <Image src={PyramidImage} alt='Pyramid image' height={262} width={262} className='hidden md:block absolute -right-36 -top-32' />
-          <Image src={TubeImage} alt='Tube image' height={248} className='hidden md:block absolute bottom-24 -left-36' />
+          <Image 
+            src={ProductImage} 
+            alt='Product image' className='mt-10' 
+          />
+          <motion.img 
+            src={PyramidImage.src} 
+            alt='Pyramid image' 
+            height={262} 
+            width={262} 
+            className='hidden md:block absolute -right-36 -top-32' 
+            style={{
+              translateY
+            }}
+          />
+          <motion.img 
+            src={TubeImage.src} 
+            alt='Tube image' 
+            height={248}
+            width={248} 
+            className='hidden md:block absolute bottom-24 -left-36' 
+            style={{
+              translateY
+            }}
+          />
         </div>
       </div>
     </section>
